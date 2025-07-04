6e50891851957d84c942d9d9159070cc8c42c561
Panagiotis Triantafyllidis <giotist@hotmail.com>
2025-07-04 09:43:45 +0300

    Add project files.

---
+++ CP_Logic_Image_Gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CP_Logic_Image_Gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
