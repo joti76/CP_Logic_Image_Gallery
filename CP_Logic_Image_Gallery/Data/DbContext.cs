686d9046c7ce3e5ee4c3a90103be60917f82d963
Panagiotis Triantafyllidis <giotist@hotmail.com>
2025-07-04 11:06:32 +0300

    Added DbContext Class

---
+++ CP_Logic_Image_Gallery/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace CP_Logic_Image_Gallery.Data
+{
+    public class DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new SqliteConnection(_connectionString);
+    }
+}
