45179c1136b443342ec2186074df703e00c7e14d
Panagiotis Triantafyllidis <giotist@hotmail.com>
2025-07-10 12:13:44 +0300

    Register Dapper

--- CP_Logic_Image_Gallery/Program.cs
+++ CP_Logic_Image_Gallery/Program.cs
@@ -1,12 +1,15 @@
 using CP_Logic_Image_Gallery.Data;
+using Microsoft.Data.Sqlite;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Register the DbContext with dependency injection
-builder.Services.AddSingleton<DbContext>();
+// ADD this line (Dapper registration)
+builder.Services.AddScoped<IDbConnection>(sp =>
+    new SqliteConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
