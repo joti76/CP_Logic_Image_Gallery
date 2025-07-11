3d464c6bf03c41716634459098e75af3200af1df
Panagiotis Triantafyllidis <giotist@hotmail.com>
2025-07-11 09:50:20 +0300

    Migration initial, scaffold crud

---
+++ CP_Logic_Image_Gallery/Controllers/FoldersController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CP_Logic_Image_Gallery.Models;
+
+namespace CP_Logic_Image_Gallery.Controllers
+{
+    public class FoldersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FoldersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Folders
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Folders.ToListAsync());
+        }
+
+        // GET: Folders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var folder = await _context.Folders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (folder == null)
+            {
+                return NotFound();
+            }
+
+            return View(folder);
+        }
+
+        // GET: Folders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Folders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Parent_Id,User_Id,Date,Enabled,Description")] Folder folder)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(folder);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(folder);
+        }
+
+        // GET: Folders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var folder = await _context.Folders.FindAsync(id);
+            if (folder == null)
+            {
+                return NotFound();
+            }
+            return View(folder);
+        }
+
+        // POST: Folders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Parent_Id,User_Id,Date,Enabled,Description")] Folder folder)
+        {
+            if (id != folder.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(folder);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FolderExists(folder.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(folder);
+        }
+
+        // GET: Folders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var folder = await _context.Folders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (folder == null)
+            {
+                return NotFound();
+            }
+
+            return View(folder);
+        }
+
+        // POST: Folders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var folder = await _context.Folders.FindAsync(id);
+            if (folder != null)
+            {
+                _context.Folders.Remove(folder);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FolderExists(int id)
+        {
+            return _context.Folders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CP_Logic_Image_Gallery/Controllers/ImagesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CP_Logic_Image_Gallery.Models;
+
+namespace CP_Logic_Image_Gallery.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Images
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Images.Include(i => i.Folder);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Images/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .Include(i => i.Folder)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // GET: Images/Create
+        public IActionResult Create()
+        {
+            ViewData["Folder_Id"] = new SelectList(_context.Folders, "Id", "Id");
+            return View();
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,User_Id,Date_Created,Folder_Id,Description,Meta_Data,Enabled")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(image);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Folder_Id"] = new SelectList(_context.Folders, "Id", "Id", image.Folder_Id);
+            return View(image);
+        }
+
+        // GET: Images/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            ViewData["Folder_Id"] = new SelectList(_context.Folders, "Id", "Id", image.Folder_Id);
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,User_Id,Date_Created,Folder_Id,Description,Meta_Data,Enabled")] Image image)
+        {
+            if (id != image.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(image);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageExists(image.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Folder_Id"] = new SelectList(_context.Folders, "Id", "Id", image.Folder_Id);
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .Include(i => i.Folder)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var image = await _context.Images.FindAsync(id);
+            if (image != null)
+            {
+                _context.Images.Remove(image);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageExists(int id)
+        {
+            return _context.Images.Any(e => e.Id == id);
+        }
+    }
+}
